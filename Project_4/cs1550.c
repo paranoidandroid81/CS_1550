a3386cee79b159cf8fc6fdabf200200958353bc0
paranoidandroid81 <mpk44@pitt.edu>
2018-12-04 11:05:39 -0500

    Fixed mkdir + bitmap

--- Project_4/cs1550.c
+++ Project_4/cs1550.c
@@ -35,6 +35,19 @@
 //How many files can there be in one directory?
 #define MAX_FILES_IN_DIR (BLOCK_SIZE - sizeof(int)) / ((MAX_FILENAME + 1) + (MAX_EXTENSION + 1) + sizeof(size_t) + sizeof(long))
 
+//for debugging, converts byte to equivalent binary w masking
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0')
+
+
 //The attribute packed means to not align these things
 struct cs1550_directory_entry
 {
@@ -88,6 +101,15 @@ struct cs1550_disk_block
 
 typedef struct cs1550_disk_block cs1550_disk_block;
 
+//struct to hold info on allocated block bitwise index and actual index on disk
+struct cs1550_free_block
+{
+	int bit_idx;
+	int actual_idx;
+};
+
+typedef struct cs1550_free_block cs1550_free_block;
+
 //helper to get the root directory from the disk
 static cs1550_root_directory* get_root(FILE* this_disk)
 {
@@ -98,54 +120,70 @@ static cs1550_root_directory* get_root(FILE* this_disk)
 }
 
 //helper to write modified block to disk
-static void write_block(FILE* this_disk, void* mod_block, int byte_idx)
+static void write_block(FILE* this_disk, void* mod_block, int block_idx)
 {
-	printf("byte idx: %d\n", byte_idx);								//DEBUG
+	//printf("block idx: %d\n", block_idx);								//DEBUG
 	//go from beginning of disk to start write index
-	fseek(this_disk, byte_idx, SEEK_SET);
-	fwrite(mod_block, BLOCK_SIZE, 1, this_disk);			//write 1 block to disk
+	fseek(this_disk, (block_idx * BLOCK_SIZE), SEEK_SET);					//byte idx on disk = block_idx * num_bytes per block
+	fwrite(mod_block, BLOCK_SIZE, 1, this_disk);
 }
 
 //helper to retrieve current bitmap from disk (stored at end)
 static void get_bitmap(FILE* this_disk, unsigned char** map)
 {
-	printf("bitmap size: %d\n", BITMAP_SIZE);
+	//printf("bitmap size: %d\n", BITMAP_SIZE);
 	fseek(this_disk, MAP_START, SEEK_SET);					//go to beginning of map from disk
 	fread(*map, sizeof(unsigned char), BITMAP_SIZE, this_disk);
 }
 
 //helper to find free block on bitmap, should skip 1st as == root
-static int* find_free(unsigned char** map)
+static cs1550_free_block* find_free(unsigned char** map)
 {
-	int* found_idx = (int*)malloc(2 * sizeof(int));						//holds both bitwise index and actual
+	cs1550_free_block* this_block = (cs1550_free_block*)malloc(sizeof(cs1550_free_block));
 	int map_idx, bit_idx;
 	unsigned char curr_bit;
+//	int bitmap_idx;
+	/*for (bitmap_idx = 0; bitmap_idx < 8; bitmap_idx++)
+	{
+		printf("curr_byte mapb4disk: " BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY((*map)[bitmap_idx]));					//DEBUG
+	}*/
 	for (map_idx = 0; map_idx < BITMAP_SIZE; map_idx++)
 	{
-		printf("Current map idx: %d\n", map_idx);					//DEBUG
+	//	printf("Current map idx: %d\n", map_idx);					//DEBUG
+	//	printf("curr_byte: " BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY((*map)[map_idx]));					//DEBUG
+		//all bits 1s, no free blocks
+		if (map_idx != 0 && ((*map)[map_idx] == 0xFF))
+		{
+			//completely full block (other than root) will be all 1s, = 0xFF
+			continue;
+		} else if (map_idx == 0 && ((*map)[map_idx] == 0x7F))
+		{
+			//first byte, since root is unused, is only 0x7F
+			continue;
+		}
 		//must iterate thru each 8 bits in char on disk map
 		for (bit_idx = 7; bit_idx >= 0; bit_idx--)
 		{
-			printf("bit idx: %d\n", bit_idx);						//DEBUG
+		//	printf("bit idx: %d\n", bit_idx);						//DEBUG
 			//ignore 1st bit, root dir
 			if (map_idx == 0 && bit_idx == 7)										continue;
 			//access single bit and mask out rest
 			unsigned char mask = 0x01 << bit_idx;			//00000001b to mask out bit in question
-			printf("curr_byte: %x\n", *map[map_idx]);					//DEBUG
-			curr_bit = *map[map_idx] & mask;
-			printf("curr_bit: %d\n", curr_bit);								//DEBUG
+
+			curr_bit = (*map)[map_idx] & mask;
+			//printf("curr_bit: " BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY(curr_bit));								//DEBUG
 			//0 bit is free, 1 bit is used
 			if (!curr_bit)
 			{
 				//we've found a free block!
 				//translate to block index for actual and bit (different)
-				found_idx[0] = (map_idx * 8) + bit_idx;				//bit index
-				found_idx[1] = (map_idx * 8) + (7 - bit_idx);	//actual index
-				return found_idx;
+				this_block->bit_idx = (map_idx * 8) + bit_idx;				//bit index
+				this_block->actual_idx = (map_idx * 8) + (7 - bit_idx);	//actual index
+				return this_block;
 			}
 		}
 	}
-	return found_idx;
+	return this_block;
 }
 
 //helper to set a particular bit on the bitmap, translates from block to map location
@@ -155,27 +193,24 @@ static void set_bit(unsigned char** map, int block_idx, unsigned char flag)
 	int map_idx, bit_idx;
 	map_idx = block_idx / 8;					//index of char on bitmap of block. int rounds down
 	bit_idx = block_idx % 8;								//where within 8 bits of char is block represented? 0-7 possible bit indices
-	printf("set_bit block_idx: %d\n", block_idx);				//DEBUG
-	printf("map_idx: %d\n", map_idx);								//DEBUG
-	printf("bit_idx = %d\n", bit_idx);							//DEBUG
-	printf("curr_byte set_bit: %x\n", *map[map_idx]);			//DEBUG
+	//printf("set_bit block_idx: %d\n", block_idx);				//DEBUG
+	//printf("map_idx: %d\n", map_idx);								//DEBUG
+	//printf("bit_idx = %d\n", bit_idx);							//DEBUG
+//	printf("curr_byte set_bit: " BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY((*map)[map_idx]));					//DEBUG
 	//if 0, must 0 out bit w/ mask, otherwise change to 1
 	unsigned char mask = 1 << bit_idx;			//00000001b to mask out bit in question
+//	printf("mask set_bit: " BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY(mask));					//DEBUG
 	if (!flag)
 	{
-		*(map[map_idx]) &= ~(mask);				//zero out bit, mask to keep rest
+		(*map)[map_idx] &= ~(mask);				//zero out bit, mask to keep rest
 	} else
 	{
-		printf("setting used flag!\n");						//DEBUG
-		*(map[map_idx]) |= mask;						//otherwise make the digit in question a one
+	//	printf("setting used flag!\n");						//DEBUG
+		(*map)[map_idx] |= mask;						//otherwise make the digit in question a one
+	//	printf("curr_byte after_set_bit flag: " BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY(*map[map_idx]));					//DEBUG
 	}
 }
 
-//helper to find index for particular block on bitmap
-static int get_map_index(int block_idx)
-{
-
-}
 
 /*
  * Called whenever the system wants to know the file attributes, including
@@ -294,7 +329,7 @@ static int cs1550_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
 	//the filler function allows us to add entries to the listing
 	//read the fuse.h file for a description (in the ../include dir)
-	//fill with generic working and back directory entries
+	//fill with generic working and prev directory entries
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
 
@@ -397,32 +432,48 @@ static int cs1550_mkdir(const char *path, mode_t mode)
 	//must find a free block on the disk
 	//retrieve the bitmap for this disk
 	unsigned char* bitmap = (unsigned char*) malloc(BITMAP_SIZE);
+	memset(bitmap, 0, BITMAP_SIZE);					//zero out bitmap before use
 	get_bitmap(disk, &bitmap);
-	printf("map size: %d\n", sizeof(bitmap));					//DEBUG
+	/*
+	int bitmap_idx;
+	for (bitmap_idx = 0; bitmap_idx < 8; bitmap_idx++)
+	{
+		printf("curr_byte mapb4disk: " BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY(bitmap[bitmap_idx]));					//DEBUG
+	}
+	*/
 	//find a free block on the disk, store both bitwise and actual
-	int* free_idx = find_free(&bitmap);
+	cs1550_free_block* free_idx = find_free(&bitmap);
 	//allocate memory for a new directory
 	cs1550_directory_entry* new_dir = (cs1550_directory_entry*)malloc(sizeof(cs1550_directory_entry));
 	//now add the new directory to the root struct
 	strcpy(root->directories[root->nDirectories].dname, directory);				//copy name of new directory into root storage
-	root->directories[root->nDirectories].nStartBlock = free_idx[1];					//add start index of block
+	root->directories[root->nDirectories].nStartBlock = free_idx->actual_idx;					//add start index of block
+	//printf("start block: %ld\n", root->directories[root->nDirectories].nStartBlock);					//DEBUG
 	root->nDirectories++;								//increment number of used directories
+	//printf("num_dirs: %d\n", root->nDirectories);								//DEBUG
 	new_dir->nFiles = 0;							//initally 0 files
 	//set the bit for the bitmap on this directory to used (1)
-	set_bit(&bitmap, free_idx[0], 1);
-	printf("actual idx: %d\n", free_idx[1]);						//DEBUG
+	set_bit(&bitmap, free_idx->bit_idx, 1);
+	//printf("actual idx: %d\n", free_idx->actual_idx);						//DEBUG
 	//now we must save the new directory block to disk and write modified root to disk
-	write_block(disk, new_dir, (free_idx[1] * BLOCK_SIZE));					//write to bytes of starting block, need to multiply by block size
+	write_block(disk, new_dir, free_idx->actual_idx);
 	write_block(disk, root, 0);					//root 1st block
 	//also must write modified bitmap to disk
+	/*
+	//DEBUG BEFORE write
+	for (bitmap_idx = 0; bitmap_idx < 8; bitmap_idx++)
+	{
+		printf("curr_byte mapb4disk: " BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY(bitmap[bitmap_idx]));					//DEBUG
+	}*/
 	fseek(disk, MAP_START, SEEK_SET);
 	fwrite(bitmap, sizeof(unsigned char), BITMAP_SIZE, disk);
 	//now can free and close stuff
 	free(new_dir);
 	free(root);
 	free(bitmap);
+	free(free_idx);
 	fclose(disk);
-	printf("done freeing....\n");			//DEBUG
+	//printf("done freeing....\n");			//DEBUG
 	return 0;						//success!
 }
 
@@ -502,16 +553,18 @@ static int cs1550_mknod(const char *path, mode_t mode, dev_t dev)
 	if (sub_dir->nFiles >= MAX_FILES_IN_DIR)								return -ENOSPC;
 	//now get bitmap, use to find free block
 	unsigned char* bitmap = (unsigned char*) malloc(BITMAP_SIZE);
+	memset(bitmap, 0, BITMAP_SIZE);						//0 out just in case
 	get_bitmap(disk, &bitmap);
-	int* free_idx = find_free(&bitmap);
+  //find a free block on the disk, store both bitwise and actual
+	cs1550_free_block* free_idx = find_free(&bitmap);
 	//now load new attributes of file into directory
 	strcpy(sub_dir->files[sub_dir->nFiles].fname, filename);
 	strcpy(sub_dir->files[sub_dir->nFiles].fext, extension);
 	sub_dir->nFiles++;
 	//set the bit for the bitmap on this block to used (1)
-	set_bit(&bitmap, free_idx[0], 1);
-	//now we must save the modified file block to disk
-	write_block(disk, sub_dir, (free_idx[1] * BLOCK_SIZE));					//write to bytes of starting block, need to multiply by block size
+	set_bit(&bitmap, free_idx->bit_idx, 1);
+	//now we must save the modified dir block to disk
+	write_block(disk, sub_dir, free_idx->actual_idx);
 	//also must write modified bitmap to disk
 	fseek(disk, MAP_START, SEEK_SET);
 	fwrite(bitmap, sizeof(unsigned char), BITMAP_SIZE, disk);
